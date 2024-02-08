ee1f27b9b84b6a2f54d49484d8c03db7626e15b6
origin <git@github.com:OljaKn/job4j_design.git>
2024-02-08 21:34:03 +0300

    PsqlStore

---
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -0,0 +1,113 @@
+package ru.job4j.grabber;
+
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store {
+
+    private Connection connection;
+
+    public PsqlStore(Properties config) {
+        try (InputStream input = PsqlStore.class.getClassLoader()
+                .getResourceAsStream("grabber.properties")) {
+            config.load(input);
+            Class.forName(config.getProperty("driver-class-name"));
+            connection = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password"));
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public void save(Post post) {
+        try (PreparedStatement statement  = connection.prepareStatement(
+                "INSERT INTO post(name, text, link, created) VALUES (?, ?, ?, ?) " + "ON CONFLICT (?) DO NOTHING",
+                Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, post.getTitle());
+            statement.setString(2, post.getDescription());
+            statement.setString(3, post.getLink());
+            statement.setTimestamp(4, Timestamp.valueOf(post.getCreaated()));
+            statement.executeUpdate();
+            try (ResultSet resultSet = statement.getGeneratedKeys()) {
+                while (resultSet.next()) {
+                    post.setId(resultSet.getInt("id"));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        List<Post> posts = new ArrayList<>();
+        try (PreparedStatement statement  = connection.prepareStatement(
+                "SELECT * FROM post")) {
+            statement.executeQuery();
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    posts.add(new Post(resultSet.getString("title"),
+                            resultSet.getString("description"),
+                            resultSet.getString("link"),
+                            resultSet.getTimestamp("created").toLocalDateTime()));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return posts;
+    }
+
+    @Override
+    public Post findById(int id) {
+        Post post = null;
+        try (PreparedStatement statement  = connection.prepareStatement(
+                "SELECT * FROM post WHERE id = ?")) {
+            statement.setInt(1, id);
+            statement.executeQuery();
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    post = new Post(resultSet.getString("title"),
+                            resultSet.getString("description"),
+                            resultSet.getString("link"),
+                            resultSet.getTimestamp("created").toLocalDateTime());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        Properties config = new Properties();
+        PsqlStore psqlStore = new PsqlStore(config);
+        HabrCareerParse habrCareerParse = new HabrCareerParse(new HabrCareerDateTimeParser());
+        try {
+            List<Post> postList = habrCareerParse.list("https://career.habr.com/vacancies?page=1");
+            for (Post post: postList) {
+                psqlStore.save(post);
+                psqlStore.findById(post.getId());
+            }
+            psqlStore.getAll();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
